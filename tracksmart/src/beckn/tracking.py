1f98e875b5cdbb6b150c7054f4c056f75bfcfc6d
Anish1614 <anishkumar1614@gmail.com>
2025-07-09 23:46:43 +0530

    adding

--- tracksmart/src/beckn/tracking.py
+++ tracksmart/src/beckn/tracking.py
@@ -1,6 +1,6 @@
 from utils.beckn_client import beckn_post
 
-BECKN_GATEWAY_URL = 'https://sandbox.becknprotocol.io/track'
+BECKN_GATEWAY_URL = 'http://localhost:5001/beckn/bap/search'
 
 def track_order(order_id):
     payload = {
@@ -11,7 +11,7 @@ def track_order(order_id):
             "city": "std:080",
             "core_version": "0.9.3",
             "bap_id": "your-bap-id",
-            "bap_uri": "https://your-bap-url.com"
+            "bap_uri": "http://localhost:5001/beckn/bap/search"
         },
         "message": {
             "order_id": order_id
